ae8215f9380dae5c241fdb1d2e33390a3bc061da
Andrew <Andre2021537@live.ytech.edu>
2019-11-14 08:22:06 -0500

    update

--- script/index.js
+++ script/index.js
@@ -7,20 +7,8 @@ const P0NAME = "Crash"
 const P0CHARA = "crashr"
 const P1NAME = "Sam"
 const P1CHARA = "saml"
-
 let playerTurn = false;
 let logging = true;
-
-let Player0;
-let Player1;
-
-let gameBox;
-let headerBox;
-let graphicsBox;
-let barsBox;
-let controlsBox;
-let outputBox;
-
 class Fighter {
   constructor(name, charaName) {
     //'contructor' is in all JS classes
@@ -48,27 +36,27 @@ class Fighter {
     console.log("Recovered!")
   }
 }
+let Player0;
+let Player1;
+let gameBox;
+let headerBox;
+let graphicsBox;
+let barsBox;
+let controlsBox;
+let outputBox;
+
+
+
+
 
 function startup() {
-  Player0 = new Fighter(P0NAME, P0CHARA);
-  Player1 = new Fighter(P1NAME, P1CHARA);
-
-  gameBox = document.getElementById('gameBox');
-  headerBox = document.getElementById('headerBox');
-  graphicsBox = document.getElementById('graphicsBox');
-  barsBox = document.getElementById('barsBox');
-  controlsBox = document.getElementById('controlsBox');
-  outputBox = document.getElementById('outputBox');
-
-  graphicsBox.innerHTML = '<img id ="' + Player0.charaName + '" src="img/' + Player0.charaName + '_idle.png" alt="' + Player0.name + '" class="fighterIMG">'
-  graphicsBox.innerHTML += '<img id ="' + Player1.charaName + '" src="img/' + Player1.charaName + '_idle.png" alt="' + Player1.name + '" class="fighterIMG">'
 
 
 
 class FighterIMG {
   constructor(alt, id) {
 
-  
+
 
 
   }
