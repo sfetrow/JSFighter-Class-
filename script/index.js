4b5594d218fe227c4689b3fd9ee4c3e16bcd89ad
Andrew <Andre2021537@live.ytech.edu>
2019-11-21 14:21:31 -0500

    HpBars

--- script/index.js
+++ script/index.js
@@ -9,7 +9,6 @@ const P0NAME = 'Crash'
 const P0CHARA = 'crashr'
 const P1NAME = 'Sam'
 const P1CHARA = 'saml'
-
 let playerTurn = false;
 let logging = true;
 let Player0;
@@ -30,6 +29,7 @@ let controlsBox;
 let outputBox;
 
 class Fighter {
+
   constructor(name, charaName) {
     //'contructor' is in all JS classes
     // It gets run immediately when a new object is created from a class
@@ -45,11 +45,15 @@ class Fighter {
 
   //this logs who attacked who
   attack(target) {
-    console.log(this.name + ' attacked ' + target.name);
+    let damageDone = Math.floor(Math.random() * 5) + 1;
+    console.log(koCheck(target, damageDone));
+    console.log(this.name + ' attacked ' + target.name + ' doing ' + damageDone + ' points of damage!');
+    console.log(target.name + ' has ' + target.hp + ' hp points left!');
   }
 
   single(target) {
     this.attack(target);
+    endTurn();
   }
 
   double(target) {
@@ -63,22 +67,9 @@ class Fighter {
   }
 }
 
-function singleAttack() {
-  if(playerTurn == false) {
-  Player0.single(Player1);
-   playerTurn = true;
-   console.log('Player turn is now true');
-}else {
-  Player1.single(Player0);
-  playerTurn = false;
-  console.log('Player turn is now false');
-}
-}
-
 function startup() {
   Player0 = new Fighter(P0NAME, P0CHARA);
   Player1 = new Fighter(P1NAME, P1CHARA);
-
   //this makes a shortcut for 'document.getElementById'
   gameBox = document.getElementById('gameBox');
   headerBox = document.getElementById('headerBox');
@@ -86,16 +77,11 @@ function startup() {
   barsBox = document.getElementById('barsBox');
   controlsBox = document.getElementById('controlsBox');
   outputBox = document.getElementById('outputBox');
-
-
   //this shows the fighter images in the graphics box
   graphicsBox.innerHTML = '<img id ="' + Player0.charaName + '" src="img/' + Player0.charaName + '_idle.png" alt="' + Player0.name + '" class="fighterIMG">'
   graphicsBox.innerHTML += '<img id ="' + Player1.charaName + '" src="img/' + Player1.charaName + '_idle.png" alt="' + Player1.name + '" class="fighterIMG">'
-
-
   console.log("My name is " + Player0.name + " and my ATK is " + Player0.atk)
   console.log("My name is " + Player1.name + " and my ATK is " + Player1.atk)
-
   showControls() //runs the showControls() function
   updateBars() //runs the updateBars() function
 }
@@ -104,51 +90,30 @@ function showControls() {
   //checks to see which players turn it is and show the apropriate controls
   if (playerTurn) {
     //show buttons for player1 and overwrites player0's controls
-    controlsBox.innerHTML = '<button type="button" name="attack" onclick="Player1.single(Player0); showControls();">Single Attack!</button>'
-    playerTurn = false;
-    updateBars();
-    koCheck(Player1, 1);
-
-
-
+    controlsBox.innerHTML = '<button type="button" name="attack" onclick="Player1.single(Player0)">Single Attack!</button>'
   } else {
     //show buttons for player0 and overwrites player1's controls
-    controlsBox.innerHTML = '<button type="button" name="attack" onclick="Player0.single(Player1); showControls();">Single Attack!</button>'
-    playerTurn = true;
-    updateBars();
-    koCheck(Player0, 1);
-
+    controlsBox.innerHTML = '<button type="button" name="attack" onclick="Player0.single(Player1)">Single Attack!</button>'
   }
 }
-
-
 
 //checks the target's HP is less than or equal to 0, Then retuns true or false.
 function koCheck(target, amount) {
+  target.hp = target.hp - amount;
   if (target.hp <= 0) {
-    console.log(target.name + " has been crushed into oblivion");
-    console.log("");
+    hideControls();
     return true;
   } else {
-    target.hp = target.hp - amount;
-    console.log(target.name + "\'s HP: " + target.hp);
-    console.log("");
     return false;
-
-
   }
-}
-
-function hideContols() {
-controlsBox.innerHTML = "";
 }
 
 function updateBars() {
   //calculates the percent of HP
   player0PercentHP = (Player0.hp / START_HP) * 100
   player1PercentHP = (Player1.hp / START_HP) * 100
-  player0PercentHP = (Player0.sp / START_SP) * 100
-  player1PercentHP = (Player1.sp / START_SP) * 100
+  player0PercentSP = (Player0.sp / START_SP) * 100
+  player1PercentSP = (Player1.sp / START_SP) * 100
 
   //Makes sure Player0's health is not greater than 100% or less than 0%
   if (player0PercentHP <= 0) {
@@ -185,21 +150,30 @@ function updateBars() {
   } else {
     player1PercentSP = player1PercentSP
   }
+
   barsBox.innerHTML = ''
-  barsBox.innerHTML += 'P0<div class="hpBar"><div style="height:' + player0PercentHP + '%; width: 100%;" id="p0HPfill" class="HPfill"></div></div>'
-  barsBox.innerHTML += '<div class="spBar"><div style="height:' + player0PercentSP + '%; width: 100%;" id="p0SPfill" class="SPfill"></div></div>'
-  barsBox.innerHTML += 'P1<div class="hpBar"><div style="height:' + player1PercentHP + '%; width: 100%;" id="p1HPfill" class="HPfill"></div></div>'
-  barsBox.innerHTML += '<div class="spBar"><div style="height:' + player1PercentSP + '%; width: 100%;" id="p1SPfill" class="SPfill"></div></div>'
+  barsBox.innerHTML += 'P0<div class="hpBar"><div style="height: 100%; width:' + player0PercentHP + '%;" id="p0HPfill" class="HPfill"></div></div>'
+  barsBox.innerHTML += '<div class="spBar"><div style="height: 100%; width:' + player0PercentSP + '%;" id="p0SPfill" class="SPfill"></div></div>'
+  barsBox.innerHTML += 'P1<div class="hpBar"><div style="height: 100%; width:' + player1PercentHP + '%;" id="p1HPfill" class="HPfill"></div></div>'
+  barsBox.innerHTML += '<div class="spBar"><div style="height: 100%; width:' + player1PercentSP + '%;" id="p1SPfill" class="SPfill"></div></div>'
 }
 
-function hideContols() {
+// EndTurn code
+function endTurn() {
+  updateBars();
+  playerTurn = !playerTurn
+  if (koCheck(Player0, 0) || koCheck(Player1, 0)){
+    hideControls();
+  }else{
+    showControls();
+  }
+}
+
+function hideControls() {
   controlsBox.innerHTML = "";
 }
 
 /*
-
 MHW = 'delicious'
-
 MHWoutput > MHWinput
-
 */
