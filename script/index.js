b90a5ec0b0517a5b0da0cb0e9426ff2e5aff4a45
Nathan <Natha2020198@live.ytech.edu>
2019-11-21 14:30:25 -0500

    Convention
    Did some Convention policing

--- script/index.js
+++ script/index.js
@@ -5,10 +5,10 @@ const DEFAULT_DEF = 5;
 const DEFAULT_TEK = 5;
 
 //sets constants names
-const P0NAME = 'Crash'
-const P0CHARA = 'crashr'
-const P1NAME = 'Sam'
-const P1CHARA = 'saml'
+const P0NAME = 'Crash';
+const P0CHARA = 'crashr';
+const P1NAME = 'Sam';
+const P1CHARA = 'saml';
 
 let playerTurn = false;
 let logging = true;
@@ -79,25 +79,25 @@ function startup() {
 
 
   //this shows the fighter images in the graphics box
-  graphicsBox.innerHTML = '<img id ="' + Player0.charaName + '" src="img/' + Player0.charaName + '_idle.png" alt="' + Player0.name + '" class="fighterIMG">'
-  graphicsBox.innerHTML += '<img id ="' + Player1.charaName + '" src="img/' + Player1.charaName + '_idle.png" alt="' + Player1.name + '" class="fighterIMG">'
+  graphicsBox.innerHTML = '<img id ="' + Player0.charaName + '" src="img/' + Player0.charaName + '_idle.png" alt="' + Player0.name + '" class="fighterIMG">';
+  graphicsBox.innerHTML += '<img id ="' + Player1.charaName + '" src="img/' + Player1.charaName + '_idle.png" alt="' + Player1.name + '" class="fighterIMG">';
 
 
-  console.log("My name is " + Player0.name + " and my ATK is " + Player0.atk)
-  console.log("My name is " + Player1.name + " and my ATK is " + Player1.atk)
+  console.log('My name is ' + Player0.name + ' and my ATK is ' + Player0.atk);
+  console.log('My name is ' + Player1.name + ' and my ATK is ' + Player1.atk);
 
-  showControls() //runs the showControls() function
-  updateBars() //runs the updateBars() function
+  showControls(); //runs the showControls() function
+  updateBars(); //runs the updateBars() function
 }
 
 function showControls() {
   //checks to see which players turn it is and show the apropriate controls
   if (playerTurn) {
     //show buttons for player1 and overwrites player0's controls
-    controlsBox.innerHTML = '<button type="button" name="attack" onclick="Player1.single(Player0)">Single Attack!</button>'
+    controlsBox.innerHTML = '<button type="button" name="attack" onclick="Player1.single(Player0)">Single Attack!</button>';
   } else {
     //show buttons for player0 and overwrites player1's controls
-    controlsBox.innerHTML = '<button type="button" name="attack" onclick="Player0.single(Player1)">Single Attack!</button>'
+    controlsBox.innerHTML = '<button type="button" name="attack" onclick="Player0.single(Player1)">Single Attack!</button>';
   }
 }
 //checks the target's HP is less than or equal to 0, Then retuns true or false.
@@ -113,43 +113,43 @@ function koCheck(target, amount) {
 
 function updateBars() {
   //calculates the percent of HP
-  player0PercentHP = (Player0.hp / START_HP) * 100
-  player1PercentHP = (Player1.hp / START_HP) * 100
-  player0PercentSP = (Player0.sp / START_SP) * 100
-  player1PercentSP = (Player1.sp / START_SP) * 100
+  player0PercentHP = (Player0.hp / START_HP) * 100;
+  player1PercentHP = (Player1.hp / START_HP) * 100;
+  player0PercentSP = (Player0.sp / START_SP) * 100;
+  player1PercentSP = (Player1.sp / START_SP) * 100;
 
   //Makes sure Player0's health is not greater than 100% or less than 0%
   if (player0PercentHP <= 0) {
-    player0PercentHP = 0
+    player0PercentHP = 0;
   } else if (player0PercentHP > 100) {
-    player0PercentHP = 100
+    player0PercentHP = 100;
   } else {
-    player0PercentHP = player0PercentHP
+    player0PercentHP = player0PercentHP;
   }
 
   //Makes sure Player1's health is not greater than 100% or less than 0%
   if (player1PercentHP <= 0) {
-    player1PercentHP = 0
+    player1PercentHP = 0;
   } else if (player1PercentHP > 100) {
-    player1PercentHP = 100
+    player1PercentHP = 100;
   } else {
-    player1PercentHP = player1PercentHP
+    player1PercentHP = player1PercentHP;
   }
 
   //Makes sure Player0's SP is not greater than 100% or less than 0%
   if (player0PercentSP <= 0) {
-    player0PercentSP = 0
+    player0PercentSP = 0;
   } else if (player0PercentSP > 100) {
-    player0PercentSP = 100
+    player0PercentSP = 100;
   } else {
     player0PercentSP = player0PercentSP
   }
 
   //Makes sure Player1's SP is not greater than 100% or less than 0%
   if (player1PercentSP <= 0) {
-    player1PercentSP = 0
+    player1PercentSP = 0;
   } else if (player1PercentSP > 100) {
-    player1PercentSP = 100
+    player1PercentSP = 100;
   } else {
     player1PercentSP = player1PercentSP
   }
@@ -172,8 +172,9 @@ function endTurn() {
 }
 
 function hideContols() {
-  controlsBox.innerHTML = "";
+  controlsBox.innerHTML = '';
 }
+
 
 
 /*
